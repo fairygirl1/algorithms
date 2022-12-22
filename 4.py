c4d2b2441a206121b428deda59e0b8d711421883
Suslova Alexandra <92153645+fairygirl1@users.noreply.github.com>
2022-12-22 23:55:33 +0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -13,12 +13,12 @@ class Solution(object):
         arr.sort()
         difference = float('inf')
         for i in range(len(arr) - 1):
-            if arr[i + 1] - arr[i] < difference:
-                difference = arr[i + 1] - arr[i]
+            if abs(arr[i + 1] - arr[i]) < difference:
+                difference = abs(arr[i + 1] - arr[i])
 
         answer = []
         for i in range(len(arr) - 1):
-            if arr[i + 1] - arr[i] == difference:
+            if abs(arr[i + 1] - arr[i]) == difference:
                 answer.append((arr[i], arr[i + 1]))
         return answer
 
